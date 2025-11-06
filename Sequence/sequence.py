6a1072eecd24ab94379f054b6428d8030f56ef20
Asad <muhammadasadk42@gmail.com>
2025-11-06 16:49:58 +0500

    ....

---
+++ Sequence/sequence.py
@@ -0,0 +1,34 @@
+from abc import ABCMeta, abstractmethod
+
+class Sequence(metaclass=ABCMeta):
+    
+    @abstractmethod
+    def __len__(self):
+        pass
+    
+    @abstractmethod
+    def __getitem__(self, j):
+        pass
+    
+    @abstractmethod
+    def __contains__(self, val):
+        for j in range(len(self)):
+            if self[j] == val:
+                return True
+        return False
+    
+    def index(self,val):
+        for j in range(len(self)):
+            if self[j] == val:
+                return j
+            raise ValueError("Value not in sequence")
+        
+    def count(self, val):
+        k=0
+        for j in range(len(self)):
+            if self[j] == val:
+                k+=1
+            return k    
+         
+        
+            
